0209ce79a1b6ef43906bfec0f88c6d112dd5a00b
Diego M. Rivera <diego.mz.rv@gmail.com>
2019-09-19 23:59:10 -0600

    inicio app

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,121 @@
+import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { GoogleCloudVisionServiceService } from '../google-cloud-vision-service.service';
+import { Router, NavigationExtras } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  
+  selectedfeature:"LABEL_DETECTION"
+
+  constructor(
+      private camera: Camera,
+      private vision: GoogleCloudVisionServiceService,
+      private route : Router,
+      public loadingController: LoadingController,
+      public alertController: AlertController
+    ) {}
+
+  async takePhoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      targetHeight: 500,
+      targetWidth: 500,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      // correctOrientation: true
+    }
+    this.camera.getPicture(options).then(async (imageData) => {
+      const loading = await this.loadingController.create({
+        message: 'Getting Results...',
+        translucent: true
+      });
+      await loading.present();
+      this.vision.getLabels(imageData,this.selectedfeature).subscribe(async (result) => {
+        console.log(result.json())
+        let navigationExtras: NavigationExtras = {
+          queryParams: {
+            special: JSON.stringify(imageData),
+            result : JSON.stringify(result.json()),
+            feature : JSON.stringify(this.selectedfeature)
+          }};
+          this.route.navigate(["showclass"],navigationExtras)
+          await loading.dismiss()
+        }, err => {
+          console.log(err);
+        });
+      }, err => {
+        console.log(err);
+      });
+    }
+
+    async selectPhoto(){
+      const options: CameraOptions = {
+        quality: 100,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE,
+        sourceType: this.camera.PictureSourceType.SAVEDPHOTOALBUM
+      }
+      this.camera.getPicture(options).then(async (imageData) => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64:
+        const loading = await this.loadingController.create({
+          message: 'Getting Results...',
+          translucent: true
+        });
+        await loading.present();
+        this.vision.getLabels(imageData,this.selectedfeature).subscribe(async (result) => {
+          let navigationExtras: NavigationExtras = {
+            queryParams: {
+              special: JSON.stringify(imageData),
+              result : JSON.stringify(result.json()),
+              feature : JSON.stringify(this.selectedfeature)
+            }};
+            this.route.navigate(["showclass"],navigationExtras)
+            await loading.dismiss()
+          }, err => {
+            console.log(err);
+          });
+      }, (err) => {
+        console.log(err)
+      })
+    }
+
+    async presentAlertConfirm() {
+      const alert = await this.alertController.create({
+        header: 'Select one option ',
+        message: 'Take Photo or Select from Galary!!!',
+        buttons: [
+          {
+            text: 'Camara',
+            role: 'camera',
+            handler: () => {
+              this.takePhoto();
+            }
+          }, {
+            text: 'Galeria',
+            role: 'gallary',
+            handler: () => {
+              this.selectPhoto();
+            }
+          }
+        ]
+      });
+      await alert.present();
+    }
+
+    radioGroupChange(event)
+    {
+      this.selectedfeature = event.detail;
+    }
+
+
+}
---
+++ src/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    googleCloudVisionAPIKey: 'AIzaSyBrx6hy2sC67ZE1tHxOD7MD28Ji7I_G0eU'
+};
